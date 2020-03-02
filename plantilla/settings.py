ccedf9c0e848ba5af18cb2fe46180f5d7f1b66d9
Enrique <enriqkeg@gmail.com>
2020-03-02 10:22:32 -0600

    Comentado de lineas imagenes

--- plantilla/settings.py
+++ plantilla/settings.py
@@ -144,7 +144,9 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
-MEDIA_URL = os.path.join(BASE_DIR, "/static/")
-MEDIA_ROOT = '/static/'
+#Descomentar despues ----
+
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+#MEDIA_URL = os.path.join(BASE_DIR, "/static/")
+#MEDIA_ROOT = '/static/'
--- plantilla/urls.py
+++ plantilla/urls.py
@@ -43,4 +43,4 @@ urlpatterns = [
     path('histSolicitus/',TemplateView.as_view(template_name="views/historialSolicitudes.html"),name='nuevo'),
     path('api/solicitudFormulario/<int:pk>',solicitudFormularioViewSet.as_view({'get': 'retrieve','put':'update','patch':'partial_update'}), name='modificar_solicutud_formulario'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
